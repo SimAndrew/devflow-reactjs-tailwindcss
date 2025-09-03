5a6effc7a54d4a3ffe1439b43fd2bdb944407b94
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-09-03 12:53:04 +0300

    add Tailwind CSS integration and update App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-	return <h1>DevFlow Landing Page</h1>;
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">DevFlow Landing Page</h1>;
+			<button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+				Submit
+			</button>
+		</>
+	);
 }
 
 export default App;
